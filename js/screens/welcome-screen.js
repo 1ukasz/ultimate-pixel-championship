df1e7b011f0f83191c3c1df8e33951036fd1fd3a
Lukasz <lukaszmiszczak92@gmail.com>
2023-04-05 19:14:54 +0200

    welcome-screen

--- js/screens/welcome-screen.js
+++ js/screens/welcome-screen.js
@@ -1,5 +1,36 @@
 export const welcomeScreen = () => {
    const welcomeStepWrapper = document.createElement('div');
-   welcomeStepWrapper.innerText = 'Welcome screen';
+   //header
+   const titleTop = document.createElement('h2');
+   const titleBottom = document.createElement('h2');
+   const textTitleTop = document.createTextNode('Ultimate Pix');
+   const appTitleBottom = document.createTextNode('Championships');
+   titleTop.appendChild(textTitleTop);
+   titleTop.classList.add('top-title');
+   titleBottom.appendChild(appTitleBottom);
+   titleBottom.classList.add('btm-title');
+
+   //swords icon
+   const swordsImg = document.createElement('img');
+   swordsImg.src = 'src/Swords.png';
+   swordsImg.alt = 'Swords image in pixel art';
+
+   //additional info
+   const addInfo = document.createElement('h3');
+   addInfo.classList.add('subtitle');
+   addInfo.innerText = 'Fill out the form to sign up for upcoming tournee.';
+
+   //button
+
+   const chooseBtn = document.createElement('button');
+   chooseBtn.classList.add('choose-btn');
+   chooseBtn.innerText = 'Choose';
+
+   //appending
+   welcomeStepWrapper.appendChild(titleTop);
+   welcomeStepWrapper.appendChild(titleBottom);
+   welcomeStepWrapper.appendChild(swordsImg);
+   e;
+   welcomeStepWrapper.appendChild(addInfo);
    return welcomeStepWrapper;
 };
